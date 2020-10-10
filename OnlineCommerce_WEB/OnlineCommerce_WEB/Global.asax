a2f5763a6d4eb4f19a78e8786de33cb864b8d44b
hamdidamar <hmdmr1@hotmail.com>
2020-10-10 15:02:40 +0300

    Entity Framework dahil edildi

---
+++ OnlineCommerce_WEB/OnlineCommerce_WEB/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="OnlineCommerce_WEB.MvcApplication" Language="C#" %>
---
+++ OnlineCommerce_WEB/OnlineCommerce_WEB/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(OnlineCommerce_WEB.Startup))]
+namespace OnlineCommerce_WEB
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
