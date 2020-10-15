5a45ba7190b4250bfb4aa8bfed3726a97e120f86
hamdidamar <hmdmr1@hotmail.com>
2020-10-15 22:47:24 +0300

    Giriş işlemleri ve kontrolleri yapıldı. Ürün sayfası yapıldı. Genel Sayfa yönlendirmeleri yapıldı.

--- OnlineCommerce_WEB/OnlineCommerce_WEB/Controllers/LoginController.cs
+++ OnlineCommerce_WEB/OnlineCommerce_WEB/Controllers/LoginController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using OnlineCommerce_WEB.Models;
+using OnlineCommerce_WEB.Models.EntityFramework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,12 +10,43 @@ namespace OnlineCommerce_WEB.Controllers
 {
     public class LoginController : Controller
     {
+        private OnlineCommerceEntities db = new OnlineCommerceEntities();
         // GET: Login
         public ActionResult Index()
         {
             return View();
         }
 
-       
+        [HttpPost]
+        public ActionResult Index(LoginEntity entity)
+        {
+            var user = (from u in db.Accounts
+                            where u.Username == entity.Username
+                        select u).Any();
+
+            if (user) // Kullanıcı adı varmı kontrolü
+            {
+                var pass = db.Accounts.Where(p => p.Username == entity.Username).FirstOrDefault<Accounts>();
+
+                if (pass.Password == entity.Password) // Veri tabanındaki şifre ile aynı mı kontrolü
+                {
+                    Response.Write("<script>alert('Giriş Başarılı')</script>");
+                    entity.IsLogin = true;
+                }
+                else
+                {
+                    Response.Write("<script>alert('Şifre Hatalı')</script>");
+                    entity.IsLogin = false;
+                }
+            }
+            else
+            {
+                Response.Write("<script>alert('Başka bir kullanıcı adı giriniz.')</script>"); // Hata Mesajı
+                entity.IsLogin = false;
+            }
+            return View();
+        }
+
+
     }
 }
---
+++ OnlineCommerce_WEB/OnlineCommerce_WEB/Models/LoginEntity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OnlineCommerce_WEB.Models
+{
+    public class LoginEntity
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public bool IsLogin { get; set; }
+    }
+}
